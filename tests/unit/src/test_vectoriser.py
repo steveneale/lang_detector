b3f8b3915508113e33e2c974f7c44548edde3d12
Steve Neale <steveneale3000@gmail.com>
2019-06-14 23:07:41 +0100

    Convert indentation
    Converted indentation in test_vectoriser.py from tabs to spaces.

--- tests/unit/src/test_vectoriser.py
+++ tests/unit/src/test_vectoriser.py
@@ -15,15 +15,15 @@ from src import Vectoriser
 
 class TestVectoriser(unittest.TestCase):
 
-	def test_gramify(self):
+    def test_gramify(self):
 
-		self.vectoriser = Vectoriser(tokenisation="gramify")
-		grams = self.vectoriser.gramify("This, is the 1st sentence.")
-		self.assertEqual(grams, ["_t", "th", "hi", "is", "s_", "_i", "is", "s_", 
-								 "_t", "th", "he", "e_", "_1", "1s", "st", "t_", 
-								 "_s", "se", "en", "nt", "te", "en", "nc", "ce", "e_"])
+        self.vectoriser = Vectoriser(tokenisation="gramify")
+        grams = self.vectoriser.gramify("This, is the 1st sentence.")
+        self.assertEqual(grams, ["_t", "th", "hi", "is", "s_", "_i", "is", "s_", 
+                                 "_t", "th", "he", "e_", "_1", "1s", "st", "t_", 
+                                 "_s", "se", "en", "nt", "te", "en", "nc", "ce", "e_"])
 
 
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
