346eb82a8602ce1f04a9a19beb9394105e986302
Steve Neale <steveneale3000@gmail.com>
2019-11-11 15:21:41 +0000

    Add docstrings to Detector class
    Added docstrings to methods in the Detector class.

--- src/detector.py
+++ src/detector.py
@@ -14,10 +14,24 @@ from src import Vectoriser
 
 class Detector(object):
 
-    def __init__(self, gram_size=2):
+    def __init__(self, gram_size: int = 2):
+        """ Initialise the Detector class
+
+        Keyword Arguments:
+            gram_size {int} -- gram size for vectorising text input (default: {2})
+        """
         self.grams = gram_size
 
-    def detect_language(self, input_text, model_path):
+    def detect_language(self, input_text: str, model_path: str):
+        """ Detect the language of a given input text
+
+        Arguments:
+            input_text {str} -- input text
+            model_path {str} -- path to a language detection model
+
+        Returns:
+            str -- language of the input text
+        """
         model = ModelIO.load_model_from_path(model_path)
         X = Vectoriser(gram_size=self.grams).transform([input_text])
         y = model.predict(X)[0]
