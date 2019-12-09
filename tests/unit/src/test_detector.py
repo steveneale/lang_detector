460d514c4b1281ca43f45165a61b322c090dccfc
Steve Neale <steveneale3000@gmail.com>
2019-12-09 23:16:08 +0000

    Add test for 'detect_language' method
    Completed tests for the Detector class by addding a test
    for the 'detect_language' method.

--- tests/unit/src/test_detector.py
+++ tests/unit/src/test_detector.py
@@ -10,7 +10,7 @@ Unit tests for 'detector.py'
 
 import unittest
 
-from mock import patch
+from mock import patch, Mock
 
 from src import Detector
 
@@ -22,6 +22,24 @@ class TestDetector(unittest.TestCase):
 
     def test_correctly_initialised_with_gram_size(self):
         self.assertEqual(self.detector.grams, 3)
+
+    @patch("src.detector.Vectoriser")
+    @patch("src.io.ModelIO.load_model_from_path")
+    def test_detect_language(self, mock_load_model, mock_vectoriser):
+        # Mock objects, classes and methods
+        mocked_model = Mock()
+        mock_load_model.return_value = mocked_model
+        mocked_vectoriser = mock_vectoriser.return_value
+        mocked_vectoriser.transform.return_value = [1, 2, 3, 4]
+        mocked_model.predict.return_value = [1, 2]
+        # Run the 'detect_language' method
+        detected = self.detector.detect_language("This is a test", "path/to/model")
+        # Make assertions
+        mock_load_model.assert_called_with("path/to/model")
+        mock_vectoriser.assert_called_with(gram_size=3)
+        mocked_vectoriser.transform.assert_called_with(["This is a test"])
+        mocked_model.predict.assert_called_with([1, 2, 3, 4])
+        self.assertEqual(detected, 1)
 
     def tearDown(self):
         del self.detector
