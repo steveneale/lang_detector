98aa909110ec3daa37abb765e24487d58ca5cc33
Steve Neale <steveneale3000@gmail.com>
2019-12-29 22:55:56 +0000

    Add Trainer class
    Added an empty Trainer class, to be populated with functionality
    for training models.

---
+++ src/trainer.py
@@ -0,0 +1,13 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+'trainer.py'
+
+Train a model for language detection.
+
+2019 Steve Neale <steveneale3000@gmail.com>
+"""
+
+
+class Trainer(object):
+    pass
