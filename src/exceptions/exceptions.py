f36964e5d8486bb47cd3eeea6a6af04c5d8ec05b
Steve Neale <steveneale3000@gmail.com>
2019-12-31 17:27:04 +0000

    Add custom exceptions
    Added a sub-package containing custom exception classes.

---
+++ src/exceptions/exceptions.py
@@ -0,0 +1,13 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+'exceptions.py'
+
+Custom exceptions used throughout 'lang_detector'.
+
+2019 Steve Neale <steveneale3000@gmail.com>
+"""
+
+
+class CSVLoadingException(Exception):
+    pass
